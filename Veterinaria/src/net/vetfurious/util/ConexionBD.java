c806250bedd0d803f7d0b8a0b2a2b46735d28e2e
Isaac <isaacabensur@gmail.com>
2013-03-04 22:22:56 -0500

    cambio de cadena conexion

---
+++ Veterinaria/src/net/vetfurious/util/ConexionBD.java
@@ -0,0 +1,26 @@
+package net.vetfurious.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+
+	public static Connection obtenerConexion() throws SQLException {
+
+		Connection con = null;
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			con = DriverManager.getConnection("jdbc:sqlserver://localhost;databaseName=Veterinaria;user=sa;password=vetuser123456");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+		
+		
+		
+	}
+	
+}
