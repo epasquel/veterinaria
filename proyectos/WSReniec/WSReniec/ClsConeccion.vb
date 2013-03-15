3e9e215a389b85ad93c9ff93a1e99aad98203ab1
libra5100 <jose.zamudio.risco@gmail.com>
2013-03-14 23:22:56 -0500

    Servicio web Reniec ASP.Net - Servicio cliente desde Eclipse - codigo para invocar el servicio en controller.cliente

---
+++ proyectos/WSReniec/WSReniec/ClsConeccion.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Data.SqlClient
+
+
+Public Class ClsConeccion
+
+    Public Function Coneccion() As SqlConnection
+        Dim cn As New SqlConnection
+        cn.ConnectionString = ConfigurationManager.ConnectionStrings("ConexionReniec").ConnectionString
+        Return cn
+    End Function
+
+End Class
